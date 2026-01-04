a1ce384d8637d4e78a9bbcf24f04cf65de94fea6
tomer <tomertocker@mail.tau.ac.il>
2026-01-04 16:48:06 +0200

    Get Homepage skeleton Done

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
+
+
+
+
+@app.route('/')
+def main():
+    return render_template("homepage.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
