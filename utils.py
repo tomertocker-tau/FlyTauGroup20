b4ac698860839e6c4e5af85d6813ba69150d52e7
tomer <tomertocker@mail.tau.ac.il>
2026-01-07 19:00:50 +0200

    Add filters for flights (unfinished)

--- utils.py
+++ utils.py
@@ -34,36 +34,67 @@ def delete(table_name: str, where: str):
     with db_cur() as cursor:
         cursor.execute(f"DELETE FROM {table_name} WHERE {where}")
 
-def select(table_name: str, columns: List[str] = None, where: str =None, group_by:str=None, cases: Dict[str,str]=None):
+def select(table_name: str,
+           columns: List[str] = None,
+           where: str =None,
+           group_by:List[str]=None,
+           cases: Dict[str,str]=None,
+           join: Tuple[str, List[str]]=None,
+           side_join: str=None):
     with db_cur() as cursor:
-        query = get_select_query(table_name, columns, where, group_by, cases)
+        query = get_select_query(table_name, columns, where, group_by, cases, join, side_join)
         cursor.execute(query)
     return cursor.fetchall()
 
 def get_select_query(table_name: str,
                      columns: List[str] = None,
                      where: str =None,
-                     group_by:str=None,
+                     group_by:List[str]=None,
                      cases: Dict[str,str]=None,
-                     join : Tuple[str,str] = None):
+                     join : Tuple[str,List[str]] = None,
+                     side_join: str = ""):
     if not columns:
-        query = f"SELECT * FROM {table_name}"
+        query = "SELECT *"
+        if cases:
+            query += " CASE"
+            for k, v in cases.items():
+                if k in ["ELSE","AS"]:
+                    continue
+                query += f" WHEN {k} THEN {v}"
+            elsecase = cases.get("ELSE")
+            if elsecase:
+                query += f" ELSE {elsecase}"
+            query += f"END AS {cases['AS']}"
+        query += f" FROM {table_name}"
+
     else:
-        query = f"SELECT {', '.join(columns)} FROM {table_name}"
+        query = f"SELECT {', '.join(columns)}"
+        if cases:
+            query += " CASE"
+            for k, v in cases.items():
+                if k in ["ELSE","AS"]:
+                    continue
+                query += f" WHEN {k} THEN {v}"
+            elsecase = cases.get("ELSE")
+            if elsecase:
+                query += f" ELSE {elsecase}"
+            query += f"END AS {cases['AS']}"
+        query += f" FROM {table_name}"
     if join:
-        query += f" JOIN {join[0]} ON {join[0]}.{join[1]} = {join[0]}.{join[1]}"
+        real_table_name = " ".split(table_name)[-1]
+        if side_join:
+            pass
+        else:
+            side_join = ""
+
+        query += f"{side_join} JOIN {join[0]} ON {join[0]}.{join[1]} = {real_table_name}.{join[1][0]}"
+        for column in join[1][1:]:
+            query += f" AND {real_table_name} {column} = {join[0]}.{column}"
     if where:
         query += f" WHERE {where}"
     if group_by:
-        query += f" GROUP BY {group_by}"
-    if cases:
-        for k, v in cases.items():
-            if k == "ELSE":
-                continue
-            query += f" WHEN {k} THEN {v}"
-        elsecase = cases.get("ELSE")
-        if elsecase:
-            query += f" ELSE {elsecase}"
+        query += f" GROUP BY {','.join(group_by)}"
+
 
 
 def check_if_admin(email: str):
@@ -81,7 +112,7 @@ def get_future_flights(include_deleted: bool = False):
                                  "Flights.TakeOffTime",
                                  "Flights.IsDeleted"
                              ],
-                             join=("FlightPrices", "FlightID"))
+                             join=("FlightPrices", ["FlightID"]))
 
     if include_deleted:
         return select(f"({query}) AS F",
@@ -111,7 +142,7 @@ def get_past_flights(include_deleted: bool = False):
                                  "Flights.TakeOffTime",
                                  "Flights.IsDeleted"
                              ],
-                             join=("FlightPrices", "FlightID"))
+                             join=("FlightPrices", ["FlightID"]))
 
     if include_deleted:
         return select(f"({query}) AS F",
@@ -170,3 +201,82 @@ def insert_phones(email: str, phones: List[str], is_signed_up: bool = True):
             continue
         else:
             insert(table_name, {"Email": email, "Phone": phone})
+
+def count_occupied_seats_query():
+    customer_query = get_select_query("CustomerOrders",
+                                      ["OrderID", "FlightID", "ClassType", "PlainID"],
+                                      join=("SelectedSeatsCustomerOrders", ["OrderID"]))
+    guests_query = get_select_query("GuestOrders",
+                                    ["OrderID", "FlightID", "ClassType", "PlainID"],
+                                    join=("SelectedSeatsGuestOrders", ["OrderID"]))
+    union_query = f"(({customer_query}) UNION ({guests_query})) AS S"
+    return get_select_query(union_query, ["S.FlightID", "S.ClassType", "S.PlainID", "COUNT(S.OrderID) AS OccupiedSeats"],
+                  group_by=["FlightID", "ClassType", "PlainID"])
+
+def get_flights_capacity_query():
+    flights_query = get_select_query("FlightPrices",
+                                     ["FlightID", "PlainID", "ClassType"],
+                                     join=("Class", ["PlainID", "ClassType"]))
+    return get_select_query(f"({flights_query}) AS FP",
+                            ["FlightID","PlainID", "ClassType", "NumberRows*NumberCols AS Capacity"])
+
+def count_available_seats_query():
+    occupied_query = count_occupied_seats_query()
+    capacity_query = get_flights_capacity_query()
+    joint_query = get_select_query(f"({occupied_query}) AS OS",
+                                   join=(f"({capacity_query}) AS CS", ["FlightID","PlainID", "ClassType"]),
+                                   side_join="Right")
+    return get_select_query(f"({joint_query}) AS Joint",
+                            ["FlightID","PlainID", "ClassType"],
+                            cases={
+                                "Joint.OccupiedSeats IS NULL": "Joint.Capacity",
+                                "ELSE": "Joint.Capacity - Joint.OccupiedSeats",
+                                "AS": "AvailableSeats"
+                            })
+
+'''def available_class_prices():
+    classes = select("Class", ["ClassType"], group_by=["ClassType"])[0]
+    '''
+
+
+
+def find_flights_by(source_field: str = None,
+                    destination_field: str = None,
+                    take_off_date: date = None,
+                    num_seats: int = None):
+    if not any([source_field, destination_field, take_off_date, num_seats]):
+        return get_future_flights()
+    else:
+        columns = ["Flights.FlightID",
+                   "Flights.SourceField",
+                   "Flights.DestinationField",
+                   "DATE(Flights.TakeOffTime)"]
+        conditions = ["IsDeleted==0"]
+        if source_field:
+            conditions.append(f"Flights.SourceField=={source_field}")
+        if destination_field:
+            conditions.append(f"Flights.DestinationField=={destination_field}")
+        if take_off_date:
+            conditions.append(f"DATE(Flights.TakeOffTime)=={take_off_date}")
+        if num_seats:
+            subquery = count_available_seats_query()
+            allquery = get_select_query("Flights",
+                                        columns,
+                                        where=" AND ".join(conditions),
+                                        join=(f"({subquery}) AS F",["FlightID"]))
+            return select(f"({allquery}) AS FF",
+                          where=" AND ".join(conditions),
+                          join=("Flights", ["FlightID"]))
+        else:
+            return select("Flights",columns, where=" AND ".join(conditions))
+
+
+
+
+
+
+
+
+
+
+
